23c4564f38407bacef6cdb53cbcf172d30fc96c6
deibytb <deiby@devacademy.la>
2017-02-19 19:50:02 -0500

    Add places in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Place.create(name: 'Plaza de Armas', latitude: -14.0663425, longitude: -75.7317949)
+Place.create(name: 'Museo Regional', latitude: -14.0730133, longitude: -75.7368589)
+Place.create(name: 'Huacachina', latitude: -14.0874944, longitude: -75.7652973)
+Place.create(name: 'El Catador', latitude: -14.0091908, longitude: -75.7566065)
+Place.create(name: 'Hacienda Bodega Tacama', latitude: -14.0018758, longitude: -75.7186927)
+Place.create(name: 'Bodega La Caravedo', latitude: -13.982664, longitude: -75.7471644)
