ffd3731daf73ff0747f3283d998b44f390da8bf3
deibytb <deiby@devacademy.la>
2017-02-20 20:39:47 -0500

    Add google maps

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,11 @@
 class PlacesController < ApplicationController
   def index
     @places = Place.all
+    @hash = Gmaps4rails.build_markers(@places) do |place, marker|
+      marker.lat place.latitude.to_s('F')
+      marker.lng place.longitude.to_s('F')
+      marker.infowindow place.name
+    end
+    p @hash.inspect + '==============='
   end
 end
